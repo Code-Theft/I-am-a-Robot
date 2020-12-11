f113679ee787fdc6bc51879d6493e919489d6d53
Code-Theft <56438939+Code-Theft@users.noreply.github.com>
2020-12-11 23:14:35 +0530

    upadte

--- freshers/assets/js/validate.js
+++ freshers/assets/js/validate.js
@@ -3,8 +3,16 @@ var score=0;
 var negscore=0;
 
 
+document.getElementById('timer').innerHTML = 002 + ":" + 40;
+startTimer();
 function startTimer() {
-  
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  if(m<0)
+  {
 	 
 	  //var total= score;
 	  //alert(total);
@@ -28,21 +36,21 @@ document.getElementById("totalneg").value=negscore;
                 data:$("#gform").serialize(),
                 method:"post",
                 success:function (response){
-					if(score==10)
+					if(score>=20)
 					{
-						alert("Form submitted successfully\n Redirecting To Next Task")
+						
+                    alert("Form submitted successfully\n<b>Hooray!! You Made to the Next Level <b>")
                     
                     window.location.replace("https://forms.gle/wexVk3U1N7UMZJoe9")
 					}
-					else{
-						
-						
-                    alert("Form submitted successfully ")
-                    
+					else
+					{
+						alert("Form submitted successfully\n OOPS!! Try again ")
+						location.reload()
 					}
-                },
+				},
                 error:function (err){
-                    alert("Something Error")
+                    alert("Something Error ")
     
                 }
             })
@@ -60,14 +68,28 @@ document.getElementById("totalneg").value=negscore;
 	
 	
 	  
+  }
   
-  
-  
+  document.getElementById('timer').innerHTML = m + ":" + s;
+  console.log(m)
+  setTimeout(startTimer, 1000);
+}
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
 }
 
+function nextimg()
+{
+	const ini=132;
+	var val=ini+i;
+	 
+	document.getElementById("icon").src="assets/img/"+val+".jfif";
+	i++;
+	
 
-
-
+}
 function nextimg_val()
 {
 	const ini=250;
@@ -113,6 +135,8 @@ function datab(k)
 	switch(k)
 	{	
 		
+		
+		
 		case 251 :	ret="UUUMUE";return ret;
 		case 252 :	ret="K8YPUT";return ret;
 		case 253 :	ret="W6UHYC";return ret;
@@ -122,9 +146,48 @@ function datab(k)
 		case 257 :	ret="8TJKA6";return ret;
 		case 258 :	ret="95K8SB";return ret;
 		case 259 :	ret="A5DP5K";return ret;
-		
-		case 260 :  startTimer();return 
-		
+		case 260 :	ret="EB66NK";return ret;
+		case 261 :	ret="CAP6T9";return ret;
+		case 262 :	ret="BVCX66";return ret;
+		case 263 :	ret="CEPHJ3";return ret;
+		case 264 :	ret="N9XM9P";return ret;
+		case 265 :	ret="AUWCBX";return ret;
+		case 266 :	ret="53KTBX";return ret;
+		case 267 :	ret="WJNW9";return ret;
+		case 268 :	ret="5RUH3S";return ret;
+		case 269 :	ret="4SN8SJ";return ret;
+		case 270 :	ret="T6XU3V";return ret;
+		case 271 :	ret="TMHX8E";return ret;
+		case 272 :	ret="X4UYNK";return ret;
+		case 273 :	ret="KPVJWC";return ret;
+		case 274 :	ret="BCTHY5";return ret;
+		case 275 :	ret="RJWMC5";return ret;
+		case 276 :	ret="CH993N";return ret;
+		case 277 :	ret="NRPJJD";return ret;
+		case 278 :	ret="XCK54T";return ret;
+		case 279 :	ret="45EVDH";return ret;
+		case 280 :	ret="SER4VX";return ret;
+		case 281 :	ret="3HA8PA";return ret;
+		case 282 :	ret="HV6NEK";return ret;
+		case 283 :	ret="E9DC53";return ret;
+		case 284 :	ret="VN3H6J";return ret;
+		case 285 :	ret="JJ3PNV";return ret;
+		case 286 :	ret="8KSWPP";return ret;
+		case 287 :	ret="TYDRA6";return ret;
+		case 288 :	ret="E3AD4X";return ret;
+		case 289 :	ret="UPWXDB";return ret;
+		case 290 :	ret="PV8BHY";return ret;
+		case 291 :	ret="SHHRU4";return ret;
+		case 292 :	ret="XU5X6V";return ret;
+		case 293 :	ret="RR3TWH";return ret;
+		case 294 :	ret="TSPEVJ";return ret;
+		case 295 :	ret="Y3DUVX";return ret;
+		case 296 :	ret="SERY3D";return ret;
+		case 297 :	ret="S5AR84";return ret;
+		case 298 :	ret="3KASAD";return ret;
+		case 299 :	ret="SNTY38";return ret;
+		case 300 :	ret="4YE9HW";return ret;
+
 		
 		
 		
